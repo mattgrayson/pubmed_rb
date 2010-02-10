60e7b418d838dc92bfba207b512f054051ccdae1
Matt Grayson <jgrayson@district-twelve.(none)>
2010-02-10 08:57:11 -0600

    Sanity check-in

--- lib/pubmed.rb
+++ lib/pubmed.rb
@@ -8,12 +8,12 @@ module PubMed
     include HTTParty
     base_uri 'http://eutils.ncbi.nlm.nih.gov/entrez/eutils/'
 
-    class Parser::XMLNokogiriDoc < HTTParty::Parser
+    class Parser::NokogiriXML < HTTParty::Parser
       def parse
-        Nokogiri.parse(body)
+        {:raw => body, :parsed => Nokogiri.parse(body)}
       end
     end
-    parser Parser::XMLNokogiriDoc
+    parser Parser::NokogiriXML
     
     attr_accessor :search_results, :last_query, :email_address, :search_uri
     
@@ -34,10 +34,11 @@ module PubMed
         :term => query
       }.update(options)
       response = self.class.get(@search_uri, :query => query_options)
+      doc = response[:parsed]
       results = {
-        :total_found => response.at(".//eSearchResult/Count") ? response.at(".//eSearchResult/Count").text : '',
-        :query_key => response.at(".//eSearchResult/QueryKey") ? response.at(".//eSearchResult/QueryKey").text : '',
-        :web_env => response.at(".//eSearchResult/WebEnv") ? response.at(".//eSearchResult/WebEnv").text : '',
+        :total_found => doc.xpath(".//eSearchResult/Count").text,
+        :query_key => doc.xpath(".//eSearchResult/QueryKey").text,
+        :web_env => doc.xpath(".//eSearchResult/WebEnv").text
       }
       if autofetch
         total = options.has_key?(:retmax) ? options[:retmax] : results[:total_found]
@@ -47,7 +48,7 @@ module PubMed
           total
         )
       else
-        results[:pmids] = response.xpath('.//eSearchResult/IdList/Id').collect {|id| id.text }
+        results[:pmids] = doc.xpath('.//eSearchResult/IdList/Id').collect(&:text)
       end
       results
     end
@@ -66,11 +67,21 @@ module PubMed
       }
       while query_options[:retstart] < total:
         puts "Fetching results #{query_options[:retstart]} - #{query_options[:retstart]+query_options[:retmax]} ..."
-        results = self.class.get(@fetch_uri, :query => query_options)      
-        articles += results["PubmedArticleSet"]["PubmedArticle"]
+        results = self.class.get(@fetch_uri, :query => query_options)
+        return results[:parsed]
+        #articles += results["PubmedArticleSet"]["PubmedArticle"]
         query_options[:retstart] += query_options[:retmax]
       end
       articles
+    end
+    
+    def extract_articles(doc)
+      articles = []
+      doc.xpath('PubmedArticleSet/PubmedArticle').each do |article|
+        a = {}
+        
+        articles << a
+      end
     end
     
   end
